dc8c77a405fbf8ab5c5b210ad7ac1c21f1732f47
Abhijeet Bhandari <abhijeetbhandari84@gmail.com>
2023-08-02 00:49:46 +0530

    added testimonials + email BE

---
+++ src/Components/Testimonials.jsx
@@ -0,0 +1,19 @@
+import "../style/Testimonials.scss";
+import Reviews from "./Reviews";
+
+function Testimonials() {
+  return (
+    <div className="testimonials">
+      <Reviews />
+      <Reviews />
+      <Reviews />
+      <Reviews />
+      <Reviews />
+      <Reviews />
+      <Reviews />
+      <Reviews />
+    </div>
+  );
+}
+
+export default Testimonials;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,12 @@ const Team = lazy(() => import("../Components/Team"));
 const Beta = lazy(() => import("../Components/Beta"));
 const Footer = lazy(() => import("../Components/Footer"));
 const Navbar = lazy(() => import("../Components/Navbar"));
+// const CardOne = lazy(() => import("../Components/CardOne"));
+// const CardTwo = lazy(() => import("../Components/CardTwo"));
+// const CardThree = lazy(() => import("../Components/CardThree"));
+const RightCardOne = lazy(() => import("../Components/RightCardOne"));
+const LifeSideCard = lazy(() => import("../Components/LeftSideCard"));
+const Testimonials = lazy(() => import("../Components/Testimonials"));
 
 const Home = () => {
   return (
@@ -22,6 +28,15 @@ const Home = () => {
         <Card />
         {/* <Clients /> */}
         {/* <Team /> */}
+        <div className="home-card">
+          <RightCardOne />
+          {/* <CardOne /> */}
+          {/* <CardTwo /> */}
+          {/* <CardThree /> */}
+          <LifeSideCard />
+        </div>
+        {/* <Stack /> */}
+        <Testimonials />
         <Beta />
         <Footer />
       </div>
